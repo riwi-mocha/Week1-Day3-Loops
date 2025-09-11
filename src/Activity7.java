a2c75059ac649b57f4980b219f2b99be3c7e9250
Johan192004 <johanruma@gmail.com>
2025-09-10 20:55:18 -0500

    feat: add functionality that generates the product table of a given number

---
+++ src/Activity7.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Activity7 {
+    
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+    
+        System.out.print("Ingrese un numero para generar la tabla de multiplicar: ");
+        int numberInserted = scanner.nextInt();
+        if(numberInserted==0){
+            System.out.println("No se puede generar la tabla de multiplicar de 0.");
+            return;
+        }
+
+        int currentProduct = 0;
+
+        System.out.println("Tabla de multiplicar del " + numberInserted);
+        for (int i = 1; i < 11; i++) {
+
+            currentProduct = numberInserted*i;
+            System.out.printf("%d * %d = %d\n",numberInserted,i,currentProduct);
+        }
+
+        scanner.close();
+
+    }
+}
